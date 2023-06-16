fc3dddcc120e26e66c22c5d98cdc4e88434ba8a6
David <david13ballesteros@gmail.com>
2023-06-16 18:41:45 +0200

    Fix to user #3

--- resources/views/space-blog.blade.php
+++ resources/views/space-blog.blade.php
@@ -4,7 +4,6 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Space Blog</title>
     <link rel="shortcut icon" href="/assets/img/icons/logo-bright.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
